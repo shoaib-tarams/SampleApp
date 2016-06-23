d92ed8f66ada29cf98066d67774015193c56c05c
Stephanie Chou <stephanie.chou@appdynamics.com>
2016-06-22 17:27:22 -0700

    added appdynamics sample app

---
+++ appdynamics-sample-app/src/public/js/appd.ts
@@ -0,0 +1,31 @@
+/**
+ * Created by stephanie.chou on 6/22/16.
+ */
+
+// class SampleAppController {
+//     constructor($scope : any,
+//                 $http,
+//                 $rootScope,
+//                 $timeout) {
+//
+//     }
+//
+//     public products = []; // this is probably a class of some kind with a variety of functions. should have its space
+//
+//     public save() : void {
+//
+//     }
+//
+//     // THESE ARE other stuff
+//
+//     public activateTab(tabNumber : number) : void {
+//
+//     }
+//
+//     public loopingGet() : void {
+//
+//     }
+//
+//
+//
+// }
