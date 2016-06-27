304515b5c52986bbcbc9d5edcee7b319a3cb9bb3
Stephanie Chou <stephanie.chou@appdynamics.com>
2016-06-27 10:42:14 -0700

    clean up code

--- sample-app/src/server.js
+++ sample-app/src/server.js
@@ -3,21 +3,16 @@ var server = express();
 var request = require('request');
 var bodyParser = require('body-parser')
 var domain = require('domain').create();
-
 var http = require('http');
-
 
 server.use( bodyParser.json() );
 server.use(bodyParser.urlencoded({     // to support URL-encoded bodies
   extended: true
 }));
-// server.use(express.json());       // to support JSON-encoded bodies
-// server.use(express.urlencoded());
 
 function setupStoreFrontCall(method, nodePath, apiRequest) {
   server.get('/' + nodePath, function (serverRequest, response) {
     var url = "http://rest:8080/SampleApp" + apiRequest;
-    // var url = 'http://localhost:' + javaPort + '/rest/appdserver' + apiRequest;
     var query = {};
     for (var key in serverRequest.query) {
       if (serverRequest.query.hasOwnProperty(key)) {
@@ -30,10 +25,8 @@ function setupStoreFrontCall(method, nodePath, apiRequest) {
       url: url
     };
 
-    var fakeData = "";
     if (method == 'POST') {
       data['form'] = query;
-      // fakeData= '[{"id":4,"name":"'+ data['form'].name + '","stock":'+ data['form'].stock +'},';
     } else if (method == 'GET') {
       data['qs'] = query;
     } else {
@@ -52,7 +45,7 @@ function setupStoreFrontCall(method, nodePath, apiRequest) {
       if (apiResponse && body) {
         response.send(body);
       } else {
-        response.send( fakeData + '{"id":1,"name":"iPad","stock":1},{"id":2,"name":"iPad","stock":100},{"id":3,"name":"cookies","stock":200}]');
+        response.send('[]');
       }
     });
   });
@@ -60,7 +53,6 @@ function setupStoreFrontCall(method, nodePath, apiRequest) {
 
 server.use(express.static(__dirname + '/public'));
 setupStoreFrontCall('GET', 'products', '/products');
-setupStoreFrontCall('GET', 'retrieve', '');
 setupStoreFrontCall('PUT', 'update', '/put');
 setupStoreFrontCall('DELETE', 'delete', '/del');
 setupStoreFrontCall('GET', 'exceptionJava', '/exception');
@@ -71,12 +63,7 @@ domain.on('error', function (err) {
 });
 
 server.post('/add', function(serverRequest, res) {
-
-
   data = JSON.stringify(serverRequest.body.params);
-
-
-  console.log(data);
 
   options = {
     host: 'rest',
