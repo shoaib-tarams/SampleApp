765db4820b633837ae4e5119c59ba9e8b4602d6f
Mark Prichard <mqprichard@gmail.com>
2016-06-27 16:24:03 -0700

    Added logging injection

--- rest/src/main/java/com/appdynamics/sample/resource/RESTExceptions.java
+++ rest/src/main/java/com/appdynamics/sample/resource/RESTExceptions.java
@@ -1,6 +1,7 @@
 package com.appdynamics.sample.resource;
 
 import com.appdynamics.sample.model.Product;
+import com.sun.jersey.spi.inject.Errors;
 
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
@@ -10,6 +11,7 @@ import javax.ws.rs.PathParam;
 import javax.ws.rs.core.MultivaluedMap;
 import javax.ws.rs.core.Response;
 import java.util.List;
+import java.util.logging.Logger;
 
 /**
  * Created by alanaanderson on 6/22/16.
@@ -20,28 +22,45 @@ public class RESTExceptions {
     @Inject
     protected EntityManager manager;
 
-    @GET
-    public Response throwException() throws Exception {
+    @Inject
+    private Logger log;
 
+    @GET
+    @Path("/java")
+    public Response throwException() throws Exception {
+        log.info("Throwing Exception...");
         try {
             throw new Exception("Forced Exception");
         } catch (Exception e) {
             // Ignore the Exception
         }
-        return Response.serverError().build();
+        //return Response.serverError().build();
+        return Response.serverError()
+                        .entity("Threw Java Exception")
+                        .build();
     }
 
     @GET
-    @Path("/slowrequest")
-    public Response slowRequest(@PathParam(value="delay") int delay) throws Exception {
+    @Path("/slow/{delay}")
+    public Response slowRequest(@PathParam("delay") int delay) throws Exception {
+        log.info("Slow transaction: delay = " + delay + " seconds");
         for (int x = 0; x < delay; ++x) Thread.sleep(1000);
-        return Response.ok().build();
+        return Response.ok()
+                    .entity("Completed transaction with " + delay + " seconds delay")
+                    .build();
     }
 
     @GET
-    @Path("/sqlexception")
+    @Path("/sql")
     public Response throwSqlException() throws Exception {
-        manager.createQuery("INSERT INTO non_existant_table (wrong_column) VALUES (1)").getResultList();
-        return Response.serverError().build();
+        log.info("Throwing SQLException...");
+        try {
+            manager.createQuery("INSERT INTO non_existant_table (wrong_column) VALUES (1)").getResultList();
+        } catch (Exception e) {
+            //Ignore the Exception
+        }
+        return Response.serverError()
+                        .entity("Threw SQL Exception")
+                        .build();
     }
 }
