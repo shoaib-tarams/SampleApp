e545c21bf477431cd5aac5f59fcdc41cbc3cbdaf
Mark Prichard <mqprichard@gmail.com>
2016-07-01 11:42:50 -0700

    Added -it flag to log tail instructions and comments

--- rest/start-machine-agent.sh
+++ rest/start-machine-agent.sh
@@ -3,6 +3,6 @@ source /env.sh
 
 echo "Starting Machine Agent with system properties: ${MACHINE_AGENT_JAVA_OPTS}"
 nohup java ${MACHINE_AGENT_JAVA_OPTS} -jar ${MACHINE_AGENT_HOME}/machineagent.jar </dev/null &>/dev/null &
-echo "To view Machine Agent log output: docker exec rest tail-machine-agent"
+echo "To view Machine Agent log output: docker exec -it rest tail-machine-agent"
 
 exit 0
