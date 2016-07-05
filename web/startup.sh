631eae2dd28beaa97f9701abca27c9a695f282f1
Stephanie Chou <stephanie.chou@appdynamics.com>
2016-07-05 16:01:05 -0700

    Sample App Server Code Clean Up and Button Functionality
    - Cleaned up Server Code
    - Added Functionality to Delete and Update Buttons
    - Added Routes. need angular-route

--- web/startup.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-
-npm install appdynamics@next
-
-$(cat /appdynamics/node-properties.txt /SampleApp/src/server.js > /SampleApp/src/_server.js && mv /SampleApp/src/_server.js /SampleApp/src/server.js)
-
-node SampleApp/src/server.js
