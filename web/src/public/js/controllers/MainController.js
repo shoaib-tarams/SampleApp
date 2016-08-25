f61b846ebdb06b7a9dc36d4278bcb57ba9a100a4
Stephanie Chou <stephanie.chou@appdynamics.com>
2016-08-24 17:05:22 -0700

    Add ApplicationId
    todo
    - make api call to get api call

--- web/src/public/js/controllers/MainController.js
+++ web/src/public/js/controllers/MainController.js
@@ -13,6 +13,10 @@
             CONTROLLER_PORT,
             CONTROLLER_SSL) {
 
+        // TODO add these constants to above
+        // CONTROLLER_USER,
+        //     CONTROLLER_ACCOUNT,
+        //     CONTROLLER_PWD
             $scope.ready = false;
 
             $scope.init = function () {
@@ -47,11 +51,36 @@
                         break;
                 }
 
-                var s = CONTROLLER_SSL == "true" ? "s" : "" ;
-                var url = "http" + s + "://" + CONTROLLER_URL + ":" + CONTROLLER_PORT + "/#/location=" + location;
+                var baseUrl = 'http://'+ CONTROLLER_URL + ":" + CONTROLLER_PORT + "/controller";
+                var basicAuthString = CONTROLLER_USER + "@" + CONTROLLER_ACCOUNT + ":" + CONTROLLER_PWD;
 
-                window.open(url, "AppDynamicsController");
+                // TODO these are used for testing. delete later
+                // var baseUrl = "http://localhost:8080/controller";
+                // var basicAuthString = "user1@customer1:welcome";
+                // var url= "http://localhost:8080/controller/#/location=" + location + "&application=" + 10;
+
+                // TODO debug this. Curl in javascript is an AJAX call. It is currently returning a cross origin error
+                //
+                $.ajax({
+                    url: baseUrl + "/restui/applicationManagerUiBean/applicationByName?applicationName=SampleApp",
+                    dataType: 'application/json',
+                    type: 'get',
+                    beforeSend: function(xhr) {
+                      xhr.setRequestHeader("Authorization", "Basic" + btoa(basicAuthString))
+                    },
+                    success: function(data) {
+                        var appId = data.id;
+                        var s = CONTROLLER_SSL == "true" ? "s" : "" ;
+                        var url = "http" + s + "://" + CONTROLLER_URL + ":" + CONTROLLER_PORT + "/#/location=" + location + "&application=" + appId;
+                        window.open(url, "AppDynamicsController");
+
+                    },
+                    error: function(data) {
+
+                    }
+                });
             };
+        
             $scope.init();
         }
     );
